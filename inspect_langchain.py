6bcd88df67359fb7e217b3d56779a1acf28e1cda
Dinesh Seervi <dineshseervi33716@gmail.com>
2026-03-29 23:09:33 +0530

    Delete inspect_langchain.py

--- inspect_langchain.py
+++
@@ -1,6 +0,0 @@
-import pkgutil
-import langchain
-print('langchain package location:', langchain.__file__)
-print('Submodules:')
-for m in pkgutil.iter_modules(langchain.__path__):
-    print('-', m.name)
